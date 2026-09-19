494073f2ded3533c445a01ee8419f7c42e4e4fdb
Dylan1-5 <dilanbricenolopez15@gmail.com>
2026-09-18 22:23:24 -0600

    Delete tests directory

--- tests/test_intents.py
+++
@@ -1,4 +0,0 @@
-from language.intents import parse_message
-
-def test_balance(): assert parse_message('cuánto dinero tengo')['intent'] == 'BALANCE'
-def test_slot_amount(): assert parse_message('juguemos slot 500') == {'intent': 'SLOT', 'slots': {'amount': 500}}
